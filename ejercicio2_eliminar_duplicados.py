58af4d2b9b679279fac99d349db08d3aed7ced61
Alberto.Orozco <88290905+albertoorozco993@users.noreply.github.com>
2024-10-27 22:39:34 -0500

    Update ejercicio2_eliminar_duplicados.py

--- ejercicio2_eliminar_duplicados.py
+++ ejercicio2_eliminar_duplicados.py
@@ -1,1 +1,7 @@
-
+with open('palabras.txt', 'r') as archivo:
+    palabras = [linea.strip() for linea in archivo.readlines()]
+palabras_unicas = list(set(palabras))
+with open('palabras_unicas.txt', 'w') as archivo_unicas:
+    for palabra in palabras_unicas:
+        archivo_unicas.write(palabra + '\n')
+print("Archivo 'palabras_unicas.txt' creado sin duplicados.")
